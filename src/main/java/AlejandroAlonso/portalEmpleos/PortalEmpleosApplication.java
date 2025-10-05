9ba88ebdb7d35158ef374f9778515cadd434db13
Alejandro Alonso <a.alejandro.dev@gmail.com>
2025-10-05 20:56:47 -0300

    seed

---
+++ src/main/java/AlejandroAlonso/portalEmpleos/PortalEmpleosApplication.java
@@ -0,0 +1,13 @@
+package AlejandroAlonso.portalEmpleos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalEmpleosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalEmpleosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AlejandroAlonso/portalEmpleos/PortalEmpleosApplicationTests.java
@@ -0,0 +1,13 @@
+package AlejandroAlonso.portalEmpleos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalEmpleosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
